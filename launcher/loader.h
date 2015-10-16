6643b3cde95340f00430cc57db2dd5d4de912eff
Filippo Brizzi <filippobrizzi@gmail.com>
2015-10-16 16:48:31 +0200

    added license to all files

--- launcher/loader.h
+++ launcher/loader.h
@@ -1,7 +1,28 @@
 /**
- * Compact Framework Core
- * 2014-2015 Emanuele Ruffaldi and Filippo Brizzi @ Scuola Superiore Sant'Anna, Pisa, Italy
- */
+Copyright 2015, Filippo Brizzi"
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@Author 
+Filippo Brizzi, PhD Student
+fi.brizzi@sssup.it
+Emanuele Ruffaldi, PhD
+e.ruffaldi@sssup.it
+
+PERCRO, (Laboratory of Perceptual Robotics)
+Scuola Superiore Sant'Anna
+via Luigi Alamanni 13D, San Giuliano Terme 56010 (PI), Italy
+*/
 #pragma once
 #include <unordered_map>
 #include "core.h"
