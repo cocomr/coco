fb68454867b1a956d46534413b7c2091138b7a61
pippo <filippobrizzi@gmail.com>
2016-11-10 09:47:20 +0100

    added allocator to use vector pool

--- core/include/coco/util/memory.hpp
+++ core/include/coco/util/memory.hpp
@@ -85,170 +85,35 @@ VectorPool<T> & VectorPool<T>::instance()
     return instance;
 }
 
-template<class T>
-class Vector
+template <class T>
+class PoolAllocator
 {
 public:
-    Vector()
-    { }
-    Vector(unsigned long count)
-    {
-        buffer_ = coco::util::VectorPool<T>::get(count);
-    }
-    Vector(unsigned long count, const T& value)
-    {
-        buffer_ = coco::util::VectorPool<T>::get(count);
-
-        buffer_->assign(count, value);
-    }
-    Vector(const std::vector<T> &vector)
-    {
-        buffer_ = coco::util::VectorPool<T>::get(vector.size());
-        buffer_->assign(vector.begin(), vector.end());
-    }
-    Vector(std::vector<T> &&vector)
-    {
-        buffer_ = coco::util::VectorPool<T>::get(vector.size());
-        *buffer_ = std::move(vector);
-    }
-    Vector(std::initializer_list<T> init)
-    {
-        buffer_ = coco::util::VectorPool<T>::get(init.size());
-        buffer_->assign(init.begin(), init.end());
-    }
-
-    T* data()
-    {
-        return buffer_->data();
-    }
-    const T* data() const
-    {
-        return buffer_->data();
-    }
-
-    T& operator[](unsigned long idx)
-    {
-        return (*buffer_)[idx];
-    }
-
-    const T& operator[](unsigned long idx) const
-    {
-        return (*buffer_)[idx];
-    }
-    // If the buffer doesn't exists undefined behavior
-    // Not this because I don't want user to modify the size of the vector.
-    // std::vector<T>& get()
-    // {
-    //      return *buffer_;
-    // }
-    // If the buffer doesn't exists undefined behavior
-    const std::vector<T>& get() const
-    {
-        return *buffer_;
-    }
-
-    /// Returns the number of elements of type T
-    unsigned long size() const
-    {
-        return buffer_ ? buffer_->size() : 0;
-    }
-
-    void resize(unsigned long count)
-    {
-        auto new_buffer = coco::util::VectorPool<T>::get(count);
-        std::copy_n(buffer_->begin(), std::min(count, size()), new_buffer->begin());
-        buffer_ = new_buffer;
-    }
-    void resize(unsigned long count, const T& value)
-    {
-        auto new_buffer = coco::util::VectorPool<T>::get(count);
-        auto it = std::copy_n(buffer_->begin(), std::min(count, size()), new_buffer->begin());
-        std::for_each(it, new_buffer->end(), [value](T &n){ n = value;});
-        buffer_ = new_buffer;
-    }
-
-    Vector clone() const
-    {
-        return Vector(*buffer_);
-    }
-
-    operator bool() const
-    {
-        if (buffer_)
-            return buffer_->size() > 0;
-        return false;
-    }
-
-private:
-    std::shared_ptr<std::vector<T> > buffer_;
-};
-
-
-#if 0
-template<class T>
-class MemoryPool
-{
-public:
-    static std::shared_ptr<T> get();
-
-private:
-    MemoryPool()
+    typedef T value_type;
+    PoolAllocator(/*ctor args*/)
+    {}
+    template <class U>
+    PoolAllocator(const PoolAllocator<U>& other)
     {}
 
-    ~MemoryPool()
+    T* allocate(std::size_t n)
     {
-        for (auto & fp : free_pool_)
-            delete fp;
+        ptr_ = VectorPool<T>::get(n);
+        return ptr_->data();
     }
 
-    static MemoryPool<T> &instance();
-
-    std::shared_ptr<T> getImpl()
+    void deallocate(T* p, std::size_t n)
     {
-        std::unique_lock<std::mutex> mlock(this->mutex_);
-        T * obj;
-        if (free_pool_.empty())
-        {
-            obj = new T();
-        }
-        else
-        {
-            obj = free_pool_.front();
-            free_pool_.pop_front();
-        }
-
-        return getSharedPtr(obj);
+        ptr_.reset();
     }
-
-    inline std::shared_ptr<T> getSharedPtr(T * t)
-    {
-        return std::shared_ptr<T>(t, [this] (T* obj)
-            {
-                std::unique_lock<std::mutex> mlock(this->mutex_);
-                this->free_pool_.push_back(obj);
-            });
-    }
-
-    std::mutex mutex_;
-    std::list<T *> free_pool_;
+private:
+    std::shared_ptr<std::vector<T> > ptr_;
 };
 
 template<class T>
-std::shared_ptr<T> MemoryPool<T>::get()
-{
-    return instance().getImpl();
-}
-
+using Vector = typename std::vector<T, PoolAllocator<T> >;
 template<class T>
-MemoryPool<T> & MemoryPool<T>::instance()
-{
-    static MemoryPool<T> instance;
-    return instance;
-}
-#endif
-
-
-
+using VectorPtr = typename std::shared_ptr<Vector<T> >;
 
 }  // end of namespace util
 
